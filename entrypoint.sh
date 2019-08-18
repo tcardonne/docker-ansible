d3fb5153a7c0486cf488fc31f64ad11d7146d408
Thomas Cardonne <t.cardonne@gmail.com>
2019-08-18 13:40:59 +0200

    Copy id_rsa.pub

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,12 @@ else
     echo "> No SSH key mounted in /ssh/id_rsa"
 fi
 
+if [ -f /ssh/id_rsa.pub ]; then
+    mkdir -p ~/.ssh/
+    cp /ssh/id_rsa.pub ~/.ssh/id_rsa.pub
+    chmod 600 ~/.ssh/id_rsa.pub
+else
+    echo "> No public SSH key mounted in /ssh/id_rsa.pub"
+fi
+
 exec "$@"
